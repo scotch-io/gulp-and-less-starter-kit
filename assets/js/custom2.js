68a68b37893efd425e8d550c3c88197087c57800
Rodrigo <rodrigo@qualitypress.com.br>
2015-10-26 14:23:04 -0200

    agora vai

--- assets/js/custom2.js
+++ assets/js/custom2.js
@@ -1,1 +1,22 @@
 console.log('script from cutsom2');
+   $(document).ready(function(){
+       $(window).bind('scroll', function() {
+       var navHeight = $( window ).height() - 70;
+             if ($(window).scrollTop() > navHeight) {
+                 $('nav').addClass('fixed');
+             }
+             else {
+                 $('nav').removeClass('fixed');
+             }
+        });
+    });
+
+$.scrollIt({
+  upKey: 38,             // key code to navigate to the next section
+  downKey: 40,           // key code to navigate to the previous section
+  easing: 'easeInCubic',      // the easing function for animation
+  scrollTime: 1000,       // how long (in ms) the animation takes
+  activeClass: 'active', // class given to the active nav element
+  onPageChange: null,    // function(pageIndex) that is called when page is changed
+  topOffset: 0           // offste (in px) for fixed top navigation
+});
