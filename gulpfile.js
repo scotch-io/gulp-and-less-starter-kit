7e15046f4ba9d30d27a7ab9a1025729f557cabea
Zzbaivong <baivong@users.noreply.github.com>
2016-02-13 00:39:39 +0700

    Update gulpfile.js
    gulp (default) => watch files
    gulp server => local server + watch files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Load Gulp
-var gulp    = require('gulp'),
-    gutil   = require('gulp-util'),
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     plugins = require('gulp-load-plugins')({
         rename: {
             'gulp-live-server': 'serve'
@@ -8,36 +8,39 @@ var gulp    = require('gulp'),
     });
 
 // Start Watching: Run "gulp"
-gulp.task('default', ['serve']);
+gulp.task('default', ['watch']);
+
+// Run "gulp server"
+gulp.task('server', ['serve', 'watch']);
 
 // Minify jQuery Plugins: Run manually with: "gulp squish-jquery"
-gulp.task('squish-jquery', function() {
-  return gulp.src('assets/js/libs/**/*.js')
-    .pipe(plugins.uglify({
+gulp.task('squish-jquery', function () {
+    return gulp.src('assets/js/libs/**/*.js')
+        .pipe(plugins.uglify({
             output: {
                 'ascii_only': true
             }
         }))
-    .pipe(plugins.concat('jquery.plugins.min.js'))
-    .pipe(gulp.dest('build'));
+        .pipe(plugins.concat('jquery.plugins.min.js'))
+        .pipe(gulp.dest('build'));
 });
 
 // Minify Custom JS: Run manually with: "gulp build-js"
-gulp.task('build-js', function() {
-  return gulp.src('assets/js/*.js')
-    .pipe(plugins.jshint())
-    .pipe(plugins.jshint.reporter('jshint-stylish'))
-    .pipe(plugins.uglify({
+gulp.task('build-js', function () {
+    return gulp.src('assets/js/*.js')
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+        .pipe(plugins.uglify({
             output: {
                 'ascii_only': true
             }
         }))
-    .pipe(plugins.concat('scripts.min.js'))
-    .pipe(gulp.dest('build'));
+        .pipe(plugins.concat('scripts.min.js'))
+        .pipe(gulp.dest('build'));
 });
 
 // Less to CSS: Run manually with: "gulp build-css"
-gulp.task('build-css', function() {
+gulp.task('build-css', function () {
     return gulp.src('assets/less/*.less')
         .pipe(plugins.plumber())
         .pipe(plugins.less())
@@ -45,9 +48,8 @@ gulp.task('build-css', function() {
             gutil.log(err);
             this.emit('end');
         })
-        .pipe(plugins.autoprefixer(
-            {
-                browsers: [
+        .pipe(plugins.autoprefixer({
+            browsers: [
                     '> 1%',
                     'last 2 versions',
                     'firefox >= 4',
@@ -58,22 +60,24 @@ gulp.task('build-css', function() {
                     'IE 10',
                     'IE 11'
                 ],
-                cascade: false
-            }
-        ))
+            cascade: false
+        }))
         .pipe(plugins.cssmin())
         .pipe(gulp.dest('build')).on('error', gutil.log);
 });
 
 // Default task
-gulp.task('serve', function() {
-    var server = plugins.serve.static('build', 8888);
-    server.start();
-    
+gulp.task('watch', function () {
     gulp.watch('assets/js/libs/**/*.js', ['squish-jquery']);
     gulp.watch('assets/js/*.js', ['build-js']);
     gulp.watch('assets/less/**/*.less', ['build-css']);
-    
+});
+
+// Folder "build" serving at http://localhost:8888
+// Should use Livereload (http://livereload.com/extensions/)
+gulp.task('serve', function () {
+    var server = plugins.serve.static('build', 8888);
+    server.start();
     gulp.watch(['assets/less/**/*.less', 'assets/js/*.js', 'assets/js/libs/**/*.js'], function (file) {
         server.notify.apply(server, [file]);
     });
